11f39d9d73e624e82dbcc2d0dc451043eec38a92
lucakreativ <lucaeckenfels@gmx.de>
2023-12-23 14:04:39 +0100

    last commit before restart

--- main.py
+++ main.py
@@ -17,64 +17,27 @@ def shallowCopy(color, piece):
         pieceCopy.append(piece[i])
     return colorCopy, pieceCopy
 
-def checkIfInCheck(color, piece, sideToMove):
-    if sideToMove == WHITE:
-        xSideToMove = BLACK
-    else:
-        xSideToMove = WHITE
+def checkIfInCheck(checkColor, checkPiece, sideToMove, xSideToMove):
+    boardsAfterMoves = offsetMoveGeneration(checkColor, checkPiece, xSideToMove, sideToMove)
+    for i in boardsAfterMoves:
+        color, piece = createBoard(checkColor, checkPiece, i)
+        for j in range(64):
+            if color[j] == sideToMove and piece[j] == KING:
+                return False
 
-    for i in range(64):
-        if color[i] == sideToMove and piece[i] == KING:
-            kingSquare = i
-            break
+    return True
 
-    for i in range(64):
-        if color[i] == sideToMove:
-            continue
-        if piece[i] == PAWN:
-            if sideToMove == WHITE:
-                if i + 7 == kingSquare and mailbox[mailbox64[i] + 9] != -1:
-                    return True
-                if i + 9 == kingSquare and mailbox[mailbox64[i] + 11] != -1:
-                    return True
-            else:
-                if i - 7 == kingSquare and mailbox[mailbox64[i] - 9] != -1:
-                    return True
-                if i - 9 == kingSquare and mailbox[mailbox64[i] - 11] != -1:
-                    return True            
-        elif color[i] == xSideToMove:
-            for j in range(offsets[piece[i]]):
-                while True:
-                    n = mailbox[mailbox64[i] + offset[piece[i]][j]]
-                    if n == -1:
-                        break
-                    if color[n] != EMPTY:
-                        if n == kingSquare:
-                            return True
-                        else:
-                            break
-                    if not slide[piece[i]]:
-                        break
-        if False:
-            for j in range(offsets[piece[i]]):
-                while True:
-                    n = mailbox[mailbox64[i] + offset[piece[i]][j]]
-                    if n == -1:
-                        break
-                    if color[n] != EMPTY:
-                        if color[n] == xSideToMove:
-                            boardAfterMoves = generateBoard(boardAfterMoves, color, piece, i, n)
-                            break
-                        if color[n] == sideToMove:
-                            break
-                    boardAfterMoves = generateBoard(boardAfterMoves, color, piece, i, n)
-                    if not slide[piece[i]]:
-                        break
+def getMoves(boardAfterMoves, color, piece, i, j, enPassant = False):
+    boardAfterMoves.append((i, j))
         
+    
+    return boardAfterMoves
 
-    return False
 
-def generateBoard(boardAfterMoves, color, piece, i, j, enPassant = False):
+def createBoard(color, piece, move):
+    enPassant = False
+    i, j = move
+
     if showMoves:
         print(getSquare(i) + " -> " + getSquare(j))
 
@@ -91,11 +54,8 @@ def generateBoard(boardAfterMoves, color, piece, i, j, enPassant = False):
         else:
             colorCopy[j + 8] = EMPTY
             pieceCopy[j + 8] = EMPTY
-    
-    if checkIfInCheck(colorCopy, pieceCopy, colorCopy[j]) == False:
-        boardAfterMoves.append((colorCopy, pieceCopy))
 
-    return boardAfterMoves
+    return (colorCopy, pieceCopy)
 
 def offsetMoveGeneration(color, piece, sideToMove, xSideToMove, enPassantCheck = False, enPassantFile = -1):
     boardAfterMoves = []
@@ -104,35 +64,35 @@ def offsetMoveGeneration(color, piece, sideToMove, xSideToMove, enPassantCheck =
             if piece[i] == PAWN:
                 if sideToMove == WHITE:
                     if color[i - 8] == EMPTY:
-                        boardAfterMoves = generateBoard(boardAfterMoves, color, piece, i, i - 8)
+                        boardAfterMoves = getMoves(boardAfterMoves, color, piece, i, i - 8)
                         if i <= 55 and color[i - 16] == EMPTY:
-                            boardAfterMoves = generateBoard(boardAfterMoves, color, piece, i, i - 16)
+                            boardAfterMoves = getMoves(boardAfterMoves, color, piece, i, i - 16)
                     if i >= 7 and color[i - 7] == xSideToMove and mailbox[mailbox64[i] - 9] != -1:
-                        boardAfterMoves = generateBoard(boardAfterMoves, color, piece, i, i - 7)
+                        boardAfterMoves = getMoves(boardAfterMoves, color, piece, i, i - 7)
                     if i >= 9 and color[i - 9] == xSideToMove and mailbox[mailbox64[i] - 11] != -1:
-                        boardAfterMoves = generateBoard(boardAfterMoves, color, piece, i, i - 9)
+                        boardAfterMoves = getMoves(boardAfterMoves, color, piece, i, i - 9)
 
                     
                     if enPassantCheck and i >= 32 and i <= 39:
                         if piece[i - 1] == PAWN and color[i - 1] == xSideToMove and enPassantFile == (i % 8) - 1 and mailbox[mailbox64[i] - 11] != -1:
-                            boardAfterMoves = generateBoard(boardAfterMoves, color, piece, i, i - 9, True)
+                            boardAfterMoves = getMoves(boardAfterMoves, color, piece, i, i - 9, True)
                         if piece[i + 1] == PAWN and color[i + 1] == xSideToMove and enPassantFile == (i % 8) + 1 and mailbox[mailbox64[i] - 9] != -1:
-                            boardAfterMoves = generateBoard(boardAfterMoves, color, piece, i, i - 7, True)
+                            boardAfterMoves = getMoves(boardAfterMoves, color, piece, i, i - 7, True)
                 else:
                     if color[i + 8] == EMPTY:
-                        boardAfterMoves = generateBoard(boardAfterMoves, color, piece, i, i + 8)
+                        boardAfterMoves = getMoves(boardAfterMoves, color, piece, i, i + 8)
                         if i <= 15 and color[i + 16] == EMPTY:
-                            boardAfterMoves = generateBoard(boardAfterMoves, color, piece, i, i + 16)
+                            boardAfterMoves = getMoves(boardAfterMoves, color, piece, i, i + 16)
                     if i <= 56 and color[i + 7] == xSideToMove and mailbox[mailbox64[i] + 9] != -1:
-                        boardAfterMoves = generateBoard(boardAfterMoves, color, piece, i, i + 7)
+                        boardAfterMoves = getMoves(boardAfterMoves, color, piece, i, i + 7)
                     if i <= 54 and color[i + 9] == xSideToMove and mailbox[mailbox64[i] + 11] != -1:
-                        boardAfterMoves = generateBoard(boardAfterMoves, color, piece, i, i + 9)
+                        boardAfterMoves = getMoves(boardAfterMoves, color, piece, i, i + 9)
 
                     if enPassantCheck and i >= 24 and i <= 31:
                         if piece[i - 1] == PAWN and color[i - 1] == xSideToMove and enPassantFile == (i % 8) - 1 and mailbox[mailbox64[i] + 7] != -1:
-                            boardAfterMoves = generateBoard(boardAfterMoves, color, piece, i, i + 7, True)
+                            boardAfterMoves = getMoves(boardAfterMoves, color, piece, i, i + 7, True)
                         if piece[i + 1] == PAWN and color[i + 1] == xSideToMove and enPassantFile == (i % 8) + 1 and mailbox[mailbox64[i] + 9] != -1:
-                            boardAfterMoves = generateBoard(boardAfterMoves, color, piece, i, i + 9, True)
+                            boardAfterMoves = getMoves(boardAfterMoves, color, piece, i, i + 9, True)
             
             else:
                 for j in range(offsets[piece[i]]):
@@ -142,16 +102,26 @@ def offsetMoveGeneration(color, piece, sideToMove, xSideToMove, enPassantCheck =
                             break
                         if color[n] != EMPTY:
                             if color[n] == xSideToMove:
-                                boardAfterMoves = generateBoard(boardAfterMoves, color, piece, i, n)
+                                boardAfterMoves = getMoves(boardAfterMoves, color, piece, i, n)
                                 break
                             if color[n] == sideToMove:
                                 break
-                        boardAfterMoves = generateBoard(boardAfterMoves, color, piece, i, n)
+                        boardAfterMoves = getMoves(boardAfterMoves, color, piece, i, n)
                         if not slide[piece[i]]:
                             break
 
     return boardAfterMoves
 
+
+def moveGeneration(color, piece, sideToMove, xSideToMove):
+    legalMoves = []
+    moves = offsetMoveGeneration(color, piece, sideToMove, xSideToMove)
+    for i in moves:
+        color2, piece2 = createBoard(color, piece, i)
+        if not checkIfInCheck(color2, piece2, sideToMove, xSideToMove):
+            legalMoves.append(i)
+
+    return legalMoves
 
 
 def printBoard(color, piece):
@@ -185,39 +155,14 @@ def printBoard(color, piece):
 def main():
     color, piece = fenToPosition("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR")
 
-    nodes = 0
-    #nodes = perft(color, piece, WHITE, 1)
-    print(nodes)
 
-    #printBoard(color, piece)
+    moves = moveGeneration(color, piece, WHITE, BLACK)
+    counter = 0
+    while counter < len(moves):
+        color2, piece2 = createBoard(color, piece, moves[counter])
+        moves2 = moveGeneration(color2, piece2, BLACK, WHITE)
+        counter += 1
 
-    moves = offsetMoveGeneration(color, piece, WHITE, BLACK)
-    print(len(moves))
-    for i in moves:
-        printBoard(i[0], i[1])
-        print("---------------------")
-
-
-
-def perft(color, piece, sideToMove, depth):
-    #print(depth)
-    if sideToMove == WHITE:
-        xSideToMove = BLACK
-    else:
-        xSideToMove = WHITE
-    nodes = 0
-    moves = offsetMoveGeneration(color, piece, sideToMove, xSideToMove)
-    if depth == 0:
-        print(len(moves))
-        return len(moves)
-    if sideToMove == WHITE:
-        sideToMove = BLACK
-    else:
-        sideToMove = WHITE
-    for i in moves:
-        nodes += perft(i[0], i[1], sideToMove, depth - 1)
-
-    return nodes
 
 if __name__ == "__main__":
     main()
